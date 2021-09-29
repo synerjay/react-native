7e8112520ec959954645c6d04f2a020475199f87
Jerome Tolentino <tolent.jay@gmail.com>
2021-09-29 19:31:51 +0900

    added textinput

--- App.js
+++ App.js
@@ -1,12 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
 
 export default function App() {
+  const [name, setName] = useState('Jerome');
+  const [age, setAge] = useState('30');
+
   return (
     <View style={styles.container}>
-      <Text>
-        Open up App.js to start working on your app! This is the text tag
+      <Text> Enter name:</Text>
+      <TextInput
+        placeholder='e.g. John Doe'
+        style={styles.input}
+        onChangeText={(value) => setName(value)}
+      />
+
+      <Text>Enter age:</Text>
+      <TextInput
+        keyboardType='numeric'
+        placeholder='e.g. 99'
+        style={styles.input}
+        onChangeText={(value) => setAge(value)}
+      />
+      <Text style={styles.result}>
+        name: {name}, ages: {age}
       </Text>
       <StatusBar style='auto' />
     </View>
@@ -19,5 +36,12 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: '#777',
+    padding: 8,
+    margin: 10,
+    width: 200,
   },
 });
